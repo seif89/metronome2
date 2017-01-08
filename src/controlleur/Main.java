4fe18eb211b14964ae4346b34cae981cc6353749
seif89 <seifeddine.jelassi@etudiant.univ-rennes1.fr>
2017-01-08 21:49:27 +0100

    test

--- src/controlleur/Main.java
+++ src/controlleur/Main.java
@@ -1,14 +1,12 @@
 package controlleur;
 
-import fr.istic.command.*;
-import fr.istic.materiel.Materiel;
-import fr.istic.materiel.MaterielImpl;
-import fr.istic.moteur.Moteur;
-import fr.istic.moteur.MoteurImpl;
-import fr.istic.vue.Adaptateur;
-import fr.istic.vue.AdapterInverse;
-import fr.istic.vue.Vue;
-import fr.istic.vue.VueImpl;
+import command.*;
+import moteur.Moteur;
+import moteur.MoteurImpl;
+import vue.Adaptateur;
+import vue.AdapterInverse;
+import vue.Vue;
+import vue.VueImpl;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
@@ -66,8 +64,8 @@ public class Main extends Application{
          UpdateTempo updatetempo = new UpdateTempo(controller);
          UpdateMesure updatemesure = new UpdateMesure(controller);
          Start start = new Start (controller);
-         
-         
+
+
          Poll poll = new Poll(adaptateur);
          Materiel.getHorloge().activerPeriodiquement(poll,(float) 0.2);
          
